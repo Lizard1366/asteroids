359f7f2b3a795bffd235fa845591f9c93d7fba37
Luke Reid <lreid@brandgage.com>
2025-08-20 15:29:58 -0400

    Asteroids in Python!

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidField import AsteroidField
+from shot import Shot
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = updatable
+
+	Player.containers = (updatable, drawable)
+
+	Shot.containers = (shots, updatable, drawable)
+	#INITIALIZE THE OBJECTS!
+	asteroid_field = AsteroidField()
+	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+	dt = 0
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		updatable.update(dt)
+
+		for asteroid in asteroids:
+			if player.collision(asteroid):
+				print('Game over!')
+				return
+			for shot in shots:
+				if shot.collision(asteroid):
+					asteroid.split()
+
+		screen.fill("black")
+
+		for item in drawable:
+			item.draw(screen)
+
+		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+	main()
